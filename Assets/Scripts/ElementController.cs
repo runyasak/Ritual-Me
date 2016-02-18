0c94b3ae7aee53927d8b2f161b596a1990d04359
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2016-02-18 16:13:20 +0700

    can swipe to object run

--- Assets/Scripts/ElementController.cs
+++ Assets/Scripts/ElementController.cs
@@ -7,13 +7,19 @@ public class ElementController : MonoBehaviour {
 	public float speedMoveAround = 20f;
 	public float speedMoveForward = 40f;
 
-	private bool isClick;
+	private bool isSwipe;
+	private bool isHit;
+
 
 	private Vector3 v;
 
+	Vector3 firstPressPos;
+	Vector3 secondPressPos;
+	Vector3 currentSwipe;
+
 	// Use this for initialization
 	void Start () {
-		isClick = false;
+		isSwipe = false;
 		v = transform.position - pivot.position;
 	}
 
@@ -24,24 +30,68 @@ public class ElementController : MonoBehaviour {
 	}
 
 	void moveForward (){
-		if (transform.position.x >= 0) {
-			transform.position += new Vector3 (speedMoveForward, speedMoveForward, 0f) * Time.deltaTime;
-		} else {
-			transform.position += new Vector3 (-speedMoveForward, speedMoveForward, 0f) * Time.deltaTime;
-		}
+		transform.position += currentSwipe * speedMoveForward * Time.deltaTime;
 	}
 		
 	void clickOnElement(){
 		if(Input.GetMouseButtonDown(0)){
 			RaycastHit2D hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (Input.mousePosition), Vector2.zero);
 			if (hit.collider.transform.tag == "Element"){
-				isClick = true;
+				isSwipe = true;
 			}
 		}
 	}
 
+	void swipe() {
+		RaycastHit2D hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (Input.mousePosition), Vector2.zero);
+		if(Input.GetMouseButtonDown(0)) {
+			//save began touch 2d point
+			firstPressPos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0);
+			if (hit.collider.transform.tag == "Element") {
+				isHit = true;
+			}
+		}
+		if(Input.GetMouseButtonUp(0))
+		{
+			//save ended touch 2d point
+			secondPressPos = new Vector3(Input.mousePosition.x,Input.mousePosition.y,0);
+
+			//create vector from the two points
+			currentSwipe = new Vector3(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y,0);
+
+			//normalize the 2d vector
+			currentSwipe.Normalize();
+			if (isHit) {
+				isSwipe = true;
+			}
+
+			Debug.Log (currentSwipe);
+
+//			//swipe upwards
+//			if(currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+//			{
+//				Debug.Log("up swipe");
+//			}
+//			//swipe down
+//			if(currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+//			{
+//				Debug.Log("down swipe");
+//			}
+//			//swipe left
+//			if(currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+//			{
+//				Debug.Log("left swipe");
+//			}
+//			//swipe right
+//			if(currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+//			{
+//				Debug.Log("right swipe");
+//			}
+		}
+	}
+
 	void moveCommand(){
-		if(!isClick){
+		if(!isSwipe){
 			moveAround ();
 		} else {
 			moveForward ();
@@ -50,11 +100,14 @@ public class ElementController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		Debug.Log (transform.position);
+//		Debug.Log (transform.position);
+		swipe();
 		moveCommand ();
-		clickOnElement();
+//		clickOnElement();
 //		Debug.DrawLine (transform.position, pivot.position);
 //		Debug.Log (Camera.main.ScreenPointToRay (Input.mousePosition));
 	}
+
+
 
 }
