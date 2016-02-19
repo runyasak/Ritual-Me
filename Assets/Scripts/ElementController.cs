4add6e6ed99375d6a3a9ecca295421c78548eb61
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2016-02-19 16:13:35 +0700

    can move multiple elements

--- Assets/Scripts/ElementController.cs
+++ Assets/Scripts/ElementController.cs
@@ -102,14 +102,13 @@ public class ElementController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		//      Debug.Log (transform.position);
+				Debug.Log (transform.position);
 		//      Debug.Log(Input.mousePosition);
+//		Debug.Log(Time.deltaTime * speedMoveAround);
 		moveCommand ();
 		//      clickOnElement();
 		//      Debug.DrawLine (transform.position, pivot.position);
 		//      Debug.Log (Camera.main.ScreenPointToRay (Input.mousePosition));
 	}
-
-
 
 }
