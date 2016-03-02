5bbc8a0bfb3071dab5217730e6b2aa194076cec0
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2016-03-03 01:44:45 +0700

    add ritual phase to master

---
+++ Assets/Scripts/RitualElement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RitualElement : MonoBehaviour {
+
+	private float rPosition;
+	private int elementID;
+	// Use this for initialization
+
+	public RitualElement(int elementID, float rPosition){
+		this.rPosition = rPosition;
+		this.elementID = elementID;
+	}
+	void Start () {
+		rPosition = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public float getRPosition(){
+		return rPosition;
+	}
+
+	public int getElementID(){
+		return elementID;
+	}
+}
---
+++ Assets/Scripts/RunnerController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunnerController : MonoBehaviour {
+	
+	private Vector3 dir;
+	private float mult;
+	public static RunnerController instance;
+
+	public GameObject LeftGauge;
+	public GameObject RightGauge;
+	// Use this for initialization
+	void Start () {
+		mult = 5f;
+		dir = new Vector3(1, 0, 0);
+	}
+
+	void move() {
+		this.transform.position += dir * Time.deltaTime * 5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		move ();
+		if(Input.GetMouseButtonDown(0)){
+			RaycastHit hit; Ray ray;
+			ray = new Ray (transform.position, new Vector3(0f,0f,1.0f));
+			if (Physics.Raycast (ray, out hit, 100)) {
+				if (hit.transform.tag == "Element"){
+//					foreach(RitualElement i in GaugeController.positionArr){
+//						Debug.Log ("FOR");
+//						if (i.getElementID() == (hit.transform.gameObject.GetInstanceID())) {
+//							Debug.Log ("IF");
+//							GaugeController.positionArr.Remove (i);
+//							Debug.Log(GaugeController.positionArr.Count);
+							Destroy (hit.transform.gameObject);
+//							Debug.Log ("BREAK");
+//							break;
+//						}
+//					}
+//					Debug.Log("ggggggggggggggggg"+GaugeController.positionArr[0].getElement().GetInstanceID());
+//					Debug.Log("hhhhhhhhhhhhhhh"+hit.transform.gameObject.GetInstanceID());
+				}
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.transform.tag == "Domain") {
+			dir = -dir;
+		}
+	}
+
+
+}
