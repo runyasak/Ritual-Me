028d02e793fe8d87f27a095404f4fe8b6ae84914
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2016-03-05 10:37:44 +0700

    rename rotate and add new element to prefab

--- Assets/Scripts/MagicCircleController.cs
+++ Assets/Scripts/MagicCircleController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Rotate : MonoBehaviour {
+public class MagicCircleController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
