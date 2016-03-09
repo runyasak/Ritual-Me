ca33535db5a8dcb2669228ee2e2c1bd7bb0aa88f
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2016-03-09 15:18:17 +0700

    edit element prefab

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public static GameController instance;
+
+	void Awake () {
+		instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void clearElement () {
+		GameObject[] temp = GameObject.FindGameObjectsWithTag("Element");
+		foreach(GameObject i in temp){
+			Destroy (i);	
+		}
+	}
+		
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.C)) { clearElement (); }
+	}
+}
