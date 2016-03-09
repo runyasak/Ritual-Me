f9af3c8b9409425210743f8557cd2b19d9813611
knewRock <who-are-you-._@hotmail.com>
2016-03-09 23:23:27 +0700

    Set BuildSetting

--- Assets/Scripts/MovieController.cs
+++ Assets/Scripts/MovieController.cs
@@ -2,20 +2,21 @@
 using System.Collections;
 
 public class MovieController : MonoBehaviour {
-	private MovieTexture movie;
+//	public MovieTexture movie;
 	public static MovieController instance;	
 	// Use this for initialization
 	void Awake(){
 		instance = this;
 	}
 	void Start () {
-		movie = GetComponent<Renderer> ().material.mainTexture as MovieTexture;
+//		movie = GetComponent<Renderer> ().material.mainTexture as MovieTexture;
+		Handheld.PlayFullScreenMovie ("ritual.mov");
 		this.GetComponent<Renderer> ().enabled = false;
 	}
 	
 	public void playRitual(){
 		this.GetComponent<Renderer> ().enabled = true;
-		movie.Play ();
+//		movie.Play ();
 //		GameObject[] temp = GameObject.FindGameObjectsWithTag("Wizard");
 //		foreach(GameObject i in temp){
 //			if (WitchController.instance != i) {
@@ -28,7 +29,7 @@ public class MovieController : MonoBehaviour {
 	public void stopRitual(){
 		Debug.Log ("Video stop");
 		this.GetComponent<Renderer> ().enabled = false;
-		movie.Stop ();
+//		movie.Stop ();
 //		GameObject[] temp = GameObject.FindGameObjectsWithTag("Wizard");
 //		foreach(GameObject i in temp) {
 //			if (WitchController.instance != i) {
