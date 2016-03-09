90aefaade1401ab58fc94569efedbf4c4b699bae
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2016-03-09 17:04:36 +0700

    can play video when ritual start (video is on gdrive)

---
+++ Assets/Scripts/MovieController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovieController : MonoBehaviour {
+	private MovieTexture movie;
+	public static MovieController instance;	
+	// Use this for initialization
+	void Awake(){
+		instance = this;
+	}
+	void Start () {
+		movie = GetComponent<Renderer> ().material.mainTexture as MovieTexture;
+		this.GetComponent<Renderer> ().enabled = false;
+	}
+	
+	public void playRitual(){
+		this.GetComponent<Renderer> ().enabled = true;
+		movie.Play ();
+//		GameObject[] temp = GameObject.FindGameObjectsWithTag("Wizard");
+//		foreach(GameObject i in temp){
+//			if (WitchController.instance != i) {
+//				i.GetComponent<WitchController> ().isFreeze = true;
+//			}
+//			Debug.Log ("isFreeze      "+      i.GetComponent<WitchController> ().isFreeze);
+//		}
+	}
+
+	public void stopRitual(){
+		this.GetComponent<Renderer> ().enabled = false;
+		movie.Stop ();
+//		GameObject[] temp = GameObject.FindGameObjectsWithTag("Wizard");
+//		foreach(GameObject i in temp) {
+//			if (WitchController.instance != i) {
+//				i.GetComponent<WitchController> ().isFreeze = false;
+////				i.GetComponent<PlayerController> ().pentacle.active = false;
+//			}
+//		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
