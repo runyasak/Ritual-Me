374d82096c6bbfe8fb8f08de7e383f39e13a688a
rookiemo <frozenprince.k@gmail.com>
2016-03-09 07:34:59 +0700

    Add start menu

---
+++ Assets/Scripts/StartGameMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGameMenu : MonoBehaviour {
+
+	public SpriteRenderer grayBG;
+
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 0;
+		grayBG.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void startGame(){
+		grayBG.gameObject.SetActive (false);
+		//Destroy (grayBG.gameObject);
+		Time.timeScale = 1;
+	}
+}
