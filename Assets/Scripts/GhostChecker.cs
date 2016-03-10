2cb6bae1588cd691c86efbb963d4e9adfffc6403
knewRock <who-are-you-._@hotmail.com>
2016-03-10 21:43:33 +0700

    Create ghost checker

---
+++ Assets/Scripts/GhostChecker.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostChecker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider coll) {
+		if (coll.name == "ghost_element(Clone)") {
+			GameObject[] wizardArr = GameObject.FindGameObjectsWithTag ("Wizard");
+			foreach(GameObject i in wizardArr){
+				i.GetComponent<WitchController> ().curHR -= 5;
+			}
+		}
+
+	}
+}
