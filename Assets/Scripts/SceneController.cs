a4bd728ed644dee37bbdd108311084a302d344e8
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2016-05-27 08:05:53 +0700

    implement playfab

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using SocketIO;
 using UnityEngine.UI;
+using PlayFab;
+using PlayFab.ClientModels;
 
 public class SceneController : MonoBehaviour {
 	
@@ -9,10 +11,16 @@ public class SceneController : MonoBehaviour {
 
 	private InputField input;
 
+	public string PlayFabId;
+
 	void Awake (){
+
+		PlayFabSettings.TitleId = "9C78";
+
 		GameObject go = GameObject.Find("SocketIO");
 		socketIO = go.GetComponent<SocketIOComponent>();
 		DontDestroyOnLoad (socketIO);
+		Debug.Log(SystemInfo.graphicsDeviceID);
 	}
 
 	void Start (){
@@ -20,6 +28,33 @@ public class SceneController : MonoBehaviour {
 //		StartCoroutine("CalltoServer");
 		GameObject inputGO = GameObject.Find("InputField");
 		input = inputGO.GetComponent<InputField> ();
+		Login (PlayFabSettings.TitleId);
+	}
+
+	void Login(string titleId) {
+		LoginWithCustomIDRequest request = new LoginWithCustomIDRequest() {
+			TitleId = titleId,
+			CreateAccount = false,
+			CustomId = SystemInfo.deviceUniqueIdentifier
+		};
+
+		PlayFabClientAPI.LoginWithCustomID(request, (result) => {
+			PlayFabId = result.PlayFabId;
+			Debug.Log("Got PlayFabID: " + PlayFabId);
+
+			if(result.NewlyCreated)
+			{
+				Debug.Log("(new account)");
+			}
+			else
+			{
+				Debug.Log("(existing account)");
+			}
+		},
+			(error) => {
+				Debug.Log("Error logging in player with custom ID:");
+				Debug.Log(error.ErrorMessage);
+			});
 	}
 
 	public void onClick(){
